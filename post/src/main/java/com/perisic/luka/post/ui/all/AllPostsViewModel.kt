711f4a8c333ac39f04fd5d5839b2de09130fc12a
Luka Perisic <lukaperisic97@gmail.com>
2021-06-21 11:04:28 +0200

    feat: added keystore and fixed first time turning on crash

--- post/src/main/java/com/perisic/luka/post/ui/all/AllPostsViewModel.kt
+++ post/src/main/java/com/perisic/luka/post/ui/all/AllPostsViewModel.kt
@@ -1,5 +1,8 @@
 package com.perisic.luka.post.ui.all
 
+import androidx.annotation.MainThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.Transformations.map
 import androidx.lifecycle.Transformations.switchMap
 import androidx.lifecycle.ViewModel
@@ -11,9 +14,11 @@ class AllPostsViewModel(
     locationRepository: LocationRepository
 ) : ViewModel() {
 
-    private val location = map(locationRepository.getRecentLocation()) {
-        Pair(it.latitude, it.longitude)
-    }
+    private val location = mapIf(
+        locationRepository.getRecentLocation(),
+        { it != null },
+        { location -> Pair(location.latitude, location.longitude) }
+    )
     private val pagedResponse = map(location) {
         postRepository.getAllPosts(it.first, it.second)
     }
@@ -22,6 +27,21 @@ class AllPostsViewModel(
 
     init {
         location.observeForever {}
+    }
+
+    @MainThread
+    private fun <X, Y> mapIf(
+        source: LiveData<X>,
+        predicate: (X?) -> Boolean,
+        mapFunction: (X) -> Y
+    ): LiveData<Y> {
+        val result = MediatorLiveData<Y>()
+        result.addSource(source) { x ->
+            if (predicate(x)) {
+                result.value = mapFunction(x)
+            }
+        }
+        return result
     }
 
 }
